15cd65353ea642103abfa54f41ebfb5750cdffec
GabrysiaZon <gabriela.zon@edu.uekat.pl>
2024-05-04 22:08:24 +0200

    testy

--- Documents/NetBeansProjects/Project/src/main/java/com/mycompany/project/Project.java
+++ Documents/NetBeansProjects/Project/src/main/java/com/mycompany/project/Project.java
@@ -11,6 +11,6 @@ package com.mycompany.project;
 public class Project {
 
     public static void main(String[] args) {
-        System.out.println("testyyy");
+        System.out.println("testowanie again");
     }
 }
